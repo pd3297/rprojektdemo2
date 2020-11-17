a1a88b5398a8cc9ccc4be7c121bd2d988d5a3304
pd3297 <aleksandra.kinga.stanczyk@gmail.com>
2020-11-17 12:05:02 +0100

    add

---
+++ pierwszy.R
@@ -0,0 +1,16 @@
+install.packages("devtools")
+library("devtools")
+
+install.packages("httr")
+install.packages("jsonlite")
+
+library(httr)
+library(jsonlite)
+
+endpoint<-"https://api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&appid=ccd2c7f8b414cadf0c4383ce0a541dc2"
+getWeather<-GET(endpoint)
+weatherText<-content(getWeather,"text")
+weatherJson<-fromJSON(weatherText,flatten=TRUE)
+weatherDF<-as.data.frame(weatherJson)
+View(weatherDF)
+##View(weatherText)
